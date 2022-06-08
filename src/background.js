5cae9eb410c4b57d9efa19ed22e599b144afbbf6
EveSquare <50450074+EveSquare@users.noreply.github.com>
2022-06-09 01:10:46 +0900

    [update] update icon, back icon

---
+++ src/background.js
@@ -0,0 +1,41 @@
+const setDetectedIcon = (sender) => {
+    chrome.action.setIcon({
+        path: {
+            "16": chrome.runtime.getURL("images/detected/icon_16.png"),
+            "32": chrome.runtime.getURL("images/detected/icon_32.png"),
+            "48": chrome.runtime.getURL("images/detected/icon_48.png"),
+            "128": chrome.runtime.getURL("images/detected/icon_128.png"),
+        },
+        tabId: sender.tab.id
+    });
+}
+
+const setDefaultIcon = (sender) => {
+    chrome.action.setIcon({
+        path: {
+            "16": chrome.runtime.getURL("images/default/icon_16.png"),
+            "32": chrome.runtime.getURL("images/default/icon_32.png"),
+            "48": chrome.runtime.getURL("images/default/icon_48.png"),
+            "128": chrome.runtime.getURL("images/default/icon_128.png"),
+        },
+        tabId: sender.tab.id
+    });
+}
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if(request.setIcon === "detected"){
+            setDetectedIcon(sender);
+            sendResponse({status: "ok"});
+            return;
+        }
+
+        if (request.setIcon === "notdetected") {
+            setDefaultIcon(sender);
+            sendResponse({status: "ok"});
+            return;
+        }
+
+        sendResponse({status: "this event not registered"});
+    }
+);
